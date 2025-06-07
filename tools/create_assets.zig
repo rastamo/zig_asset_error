0b216dff36cbd60f29d37f2e7fdd4426cd12d3a3
Joakim Rastamo <rastamo@vivaldi.net>
2025-06-07 23:11:38 +0200

    Add more assets

--- tools/create_assets.zig
+++ tools/create_assets.zig
@@ -3,10 +3,10 @@ const rl = @import("raylib");
 
 pub fn main() !void {
     var i: i32 = 1000;
-    var buf: [32]u8 = undefined;
+    var buf: [64]u8 = undefined;
     while (i < 20000) : (i += 2000) {
-        const image = rl.genImageColor(i, i, .red);
-        const filename = try std.fmt.bufPrintZ(&buf, "assets/image{d}.png", .{i});
+        const image = rl.genImageColor(i, i, .blue);
+        const filename = try std.fmt.bufPrintZ(&buf, "assets/textures/blue/image{d}.png", .{i});
         _ = rl.exportImage(image, filename);
     }
 }
